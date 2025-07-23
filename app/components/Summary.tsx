c3aad9f811daf459f4708fa1a08a948de3b1e1d8
OmarChouchane <omar.ch52831@gmail.com>
2025-07-23 17:27:58 +0100

    Refactor Summary component to display detailed scores
    Updated the Summary component to show overall and category-specific scores using new ScoreGauge and ScoreBadge components. Added a Category subcomponent for displaying individual category scores with color coding. The component now accepts a feedback prop to render dynamic data.

--- app/components/Summary.tsx
+++ app/components/Summary.tsx
@@ -1,14 +1,49 @@
 import React from 'react'
+import ScoreGauge from './ScoreGauge'
+import ScoreBadge from './ScoreBadge';
 
-const Summary = () => {
+const Category = ({ title, score }: { title: string; score: number }) => {
+
+    const textColor = score >= 80 ? 'text-green-500' : score >= 50 ? 'text-yellow-500' : 'text-red-500';
+
+    return (
+        <div className="resume-summary">
+            <div className="category">
+                <div className='flex flex-row gap-2 items-center'>
+                    <p className='text-2xl'>{title}</p>
+                    <ScoreBadge score={score} />
+                </div>
+                <p className='text-2xl'><span className={textColor}>{score}</span>/100</p>
+            </div>
+        </div>
+    )
+}
+
+const Summary = ({ feedback }: { feedback: Feedback | null }) => {
   return (
-    <div>
-      <h3 className="text-2xl !text-black font-bold">Summary</h3>
-      <p className="text-gray-700">
-        This section provides a summary of your resume and its key points.
-      </p>
+    <div className="bg-white rounded-2xl shadow-md w-full">
+      <div className="flex flex-row items-center p-4 gap-8">
+        <ScoreGauge score={feedback?.overallScore ?? 0} />
+        <div className="flex flex-col gap-2">
+          <div className="text-2xl font-bold">Your Resume Score</div>
+          <p className="text-sm text-gray-500">
+            This score is calculated based on the variaables listed below.
+          </p>
+        </div>
+      </div>
+
+      <Category
+        title="Tone & Style"
+        score={feedback?.toneAndStyle.score ?? 0}
+      />
+      <Category title="Content" score={feedback?.content.score ?? 0} />
+      <Category title="Structure" score={feedback?.structure.score ?? 0} />
+      <Category
+        title="Skills"
+        score={feedback?.skills.score ?? 0}
+      />
     </div>
-  )
+  );
 }
 
 export default Summary
